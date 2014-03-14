2755f6122eba5fe0d9bbcd36ab73f65b2c31a7cb
黎静北 <jingbei.li@yahoo.com>
2014-03-14 13:52:08 +0800

    lab2完成

--- labcodes/lab2/kern/mm/pmm.c
+++ labcodes/lab2/kern/mm/pmm.c
@@ -380,6 +380,16 @@ get_pte(pde_t *pgdir, uintptr_t la, bool create) {
     }
     return NULL;          // (8) return page table entry
 #endif
+	pde_t *pdep = pgdir + PDX(la);
+	if (!(*pdep & PTE_P)){
+		if (!create) return NULL;
+		struct Page* p = alloc_page();
+		set_page_ref(p, 1);
+		uintptr_t pa = page2pa(p);
+		memset(KADDR(pa), 0, 4096);
+		*pdep = pa | PTE_U | PTE_W | PTE_P;
+	}
+	return (pte_t*)KADDR(PDE_ADDR(*pdep))+PTX(la);
 }
 
 //get_page - get related Page struct for linear address la using PDT pgdir
@@ -425,6 +435,13 @@ page_remove_pte(pde_t *pgdir, uintptr_t la, pte_t *ptep) {
                                   //(6) flush tlb
     }
 #endif
+	if (*ptep & PTE_P){
+		struct Page *p = pte2page(*ptep);
+		page_ref_dec(p);
+		if (page_ref(p) == 0) free_page(p);
+		*ptep = 0;
+		tlb_invalidate(pgdir, la);
+	}
 }
 
 //page_remove - free an Page which is related linear address la and has an validated pte
