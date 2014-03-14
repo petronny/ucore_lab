9d327399f0f293645b98ec5dbb2bd3fcc77f7ce3
黎静北 <jingbei.li@yahoo.com>
2014-03-14 13:28:09 +0800

    lab2中的lab1移植

--- labcodes/lab2/kern/debug/kdebug.c
+++ labcodes/lab2/kern/debug/kdebug.c
@@ -3,10 +3,7 @@
 #include <stab.h>
 #include <stdio.h>
 #include <string.h>
-#include <sync.h>
 #include <kdebug.h>
-#include <kmonitor.h>
-#include <assert.h>
 
 #define STACKFRAME_DEPTH 20
 
@@ -291,6 +288,14 @@ read_eip(void) {
  * Note that, the length of ebp-chain is limited. In boot/bootasm.S, before jumping
  * to the kernel entry, the value of ebp has been set to zero, that's the boundary.
  * */
+
+static __noinline uint32_t
+read_ss(uint32_t addr) {
+    uint32_t val;
+    asm volatile("movl (%1), %0" : "=r" (val): "r" (addr));
+    return val;
+}
+
 void
 print_stackframe(void) {
      /* LAB1 YOUR CODE : STEP 1 */
@@ -305,5 +310,19 @@ print_stackframe(void) {
       *           NOTICE: the calling funciton's return addr eip  = ss:[ebp+4]
       *                   the calling funciton's ebp = ss:[ebp]
       */
+
+	uint32_t ebp=read_ebp();
+	uint32_t eip=read_eip();
+	int i;
+	for (i = 0; i < STACKFRAME_DEPTH && ebp != 0;i++){
+		cprintf("ebp:0x%08x eip:0x%08x args:",ebp,eip);
+		int j;
+		for(j=0;j<4;j++)
+			cprintf("0x%08x ",read_ss(ebp+4*(j+2)));
+		cprintf("\n");
+		print_debuginfo(eip-1);
+		eip=read_ss(ebp+4);
+		ebp=read_ss(ebp);
+	}
 }
 
